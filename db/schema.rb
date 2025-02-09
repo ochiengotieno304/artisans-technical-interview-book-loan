c9400be6b8404ec57e7c90a7ca36f21def233e78
ochieng otieno <ochiengotieno71@gmail.com>
2025-02-09 23:04:40 +0300

    feat: genrate books scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_09_193048) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_09_194706) do
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "isbn"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["isbn"], name: "index_books_on_isbn", unique: true
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
